f77d3ad02409d8ee2236f828e61755e063e62322
Peder <pederbsa@stud.ntnu.no>
2023-03-28 16:18:41 +0200

    first commit

---
+++ tx.py
@@ -0,0 +1,6 @@
+from unetpy import UnetSocket
+
+s = UnetSocket('129.241.10.204', 1100) # for actual modem API port is 1100
+#s = UnetSocket('localhost', 1101)  #simulation API port
+s.send('Hello Underwater!', 0)
+s.close()
